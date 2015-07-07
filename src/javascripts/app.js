84557a4504ad7562996471d4990e91b4925390de
Salman <salman.karim@hotmail.com>
2015-07-07 15:40:34 +0100

    defines app.js

---
+++ src/javascripts/app.js
@@ -0,0 +1,26 @@
+thermostat = new Thermostat();
+
+var temp = document.getElementById("temp");
+
+temp.innerHTML = thermostat.temperature;
+
+var up = element("up-button");
+
+up.onClick = thermostat.increase;
+
+// function myFunction() {
+//   document.getElementById("temp").innerHTML = thermostat.temperature;
+// }
+
+// function increase() {
+//   document.getElementById("temp").innerHTML = thermostat.increase();
+// }
+//
+
+
+
+
+// function myFunction() {
+//   document.getElementById("temp").innerHTML = thermostat.temperature;
+//
+// }
